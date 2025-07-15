8134aadf5d04b1733824baebc13925d2f879d263
Yeom-Seung-Ah <ysa2780@naver.com>
2025-07-15 22:19:23 +0900

    feat: 마이페이지 좌측 네비게이션 구현

--- src/features/myPage/components/SideMenu.tsx
+++ src/features/myPage/components/SideMenu.tsx
@@ -1,28 +1,71 @@
+import {
+  TbUser,
+  TbUserFilled,
+  TbStar,
+  TbStarFilled,
+  TbClockHour4,
+  TbClockHour4Filled,
+} from 'react-icons/tb';
 import { NavLink } from 'react-router-dom';
 
-export default function SideMenu() {
-  const menuItems = [
-    { to: '/mypage/info', label: '내 정보' },
-    { to: '/mypage/favorites', label: '찜한 혜택' },
-    { to: '/mypage/history', label: '이용 내역' },
-  ];
+const menuItems = [
+  {
+    to: '/mypage/info',
+    label: '나의 회원 정보',
+    icon: TbUser, // inactive icon
+    activeIcon: TbUserFilled, // active icon
+  },
+  {
+    to: '/mypage/favorites',
+    label: '내가 찜한 혜택',
+    icon: TbStar,
+    activeIcon: TbStarFilled,
+  },
+  {
+    to: '/mypage/history',
+    label: '혜택 사용 이력',
+    icon: TbClockHour4,
+    activeIcon: TbClockHour4Filled,
+  },
+];
 
+export default function SideMenu() {
   return (
     <aside className="w-[370px] bg-white rounded-[18px] drop-shadow-basic pt-[76px] pb-[56px] px-3">
-      <nav className="flex flex-col gap-4">
-        {menuItems.map((item) => (
-          <NavLink
-            key={item.to}
-            to={item.to}
-            className={({ isActive }) =>
-              `block p-3 rounded-lg text-center font-medium ${
-                isActive ? 'bg-purple04 text-white' : 'bg-grey02 text-grey06'
-              }`
-            }
-          >
-            {item.label}
-          </NavLink>
-        ))}
+      <h1 className="text-title-1 text-purple06 pl-[34px] pb-[80px]">My Page</h1>
+      <nav className="flex flex-col gap-5">
+        {menuItems.map((item) => {
+          const Icon = item.icon;
+          const ActiveIcon = item.activeIcon;
+          return (
+            <NavLink
+              key={item.to}
+              to={item.to}
+              className={({ isActive }) =>
+                `flex items-center h-[66px] gap-3 p-3 rounded-[10px] ${
+                  isActive
+                    ? 'bg-purple04 text-white text-title-4'
+                    : 'bg-white text-grey05 text-[24px]'
+                }`
+              }
+            >
+              {({ isActive }) => (
+                <>
+                  {isActive ? (
+                    <div className="pl-3 pr-1">
+                      <ActiveIcon size={24} strokeWidth={1} />
+                    </div>
+                  ) : (
+                    <div className="pl-3 pr-1">
+                      <Icon size={24} strokeWidth={1} />
+                    </div>
+                  )}
+                  <span>{item.label}</span>
+                </>
+              )}
+            </NavLink>
+          );
+        })}
       </nav>
     </aside>
   );
