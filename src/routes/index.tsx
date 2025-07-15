49e3b8f9158516ddb629a36acdacf59c67d1ace1
Yeom-Seung-Ah <ysa2780@naver.com>
2025-07-15 12:24:33 +0900

    feat: defaultlayout에서 랜딩페이지 제외

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -9,10 +9,10 @@ import AllBenefitsPage from '../pages/AllBenefitsPage';
 import NotFoundPage from '../pages/NotFoundPage';
 
 const router = createBrowserRouter([
+  { path: '/', element: <LandingPage /> }, // 기본 라우터
   {
     element: <DefaultLayout />,
     children: [
-      { path: '/', element: <LandingPage /> }, // 기본 라우터
       { path: '/main', element: <MainPage /> }, // 각 페이지별 레이아웃이나 자식 라우팅은 각자 작업하면서 자유롭게 추가
       { path: '/admin', element: <AdminPage /> },
       { path: '/mypage', element: <MyPage /> },
