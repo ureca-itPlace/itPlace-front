9ef0a062fbfb47daa16b4b86f90bfa570a9a7a8c
박용규 <yoyo5411@naver.com>
2025-07-16 16:37:10 +0900

    styles : modal 내용 수정

--- src/features/loginPage/constants/modalPresets.ts
+++ src/features/loginPage/constants/modalPresets.ts
@@ -4,7 +4,7 @@ export const modalPresets = {
   alreadyJoined: (goLogin: () => void, close: () => void) =>
     ({
       open: true,
-      title: '이미 가입된 계정이에요',
+      title: '이미 가입된 계정이에요요',
       subMessage: '',
       subMessageClass: '',
       buttons: [{ label: '로그인', onClick: goLogin, type: 'primary' }],
@@ -15,9 +15,8 @@ export const modalPresets = {
     ({
       open: true,
       title: '계정 통합 안내내',
-      message: '입력하신 정보로 기존 가입 내역이 확인되었습니다.',
-      subMessage:
-        '통합을 진행하면 기존 계정의 정보가 유지되며\n앞으로는 현재 로그인 방식으로 편리하게 서비스를 이용하실 수 있습니다.',
+      message: '입력하신 정보로',
+      subMessage: '',
       subMessageClass: '',
       buttons: [{ label: '통합하기', onClick: onMerge, type: 'primary' }],
     }) as ModalState,
