92610aa083b76625a29fec361126b028647a1f0c
Yeom-Seung-Ah <ysa2780@naver.com>
2025-07-17 18:38:08 +0900

    feat: ToastProvider를 앱시작 부분에 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,15 @@
 import './App.css';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes';
+import ToastProvider from './components/ToastProvider';
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <RouterProvider router={router} />
+      <ToastProvider />
+    </>
+  );
 };
 
 export default App;
