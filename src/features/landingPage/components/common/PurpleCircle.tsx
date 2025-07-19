3382d87d9c2a070bc220cb20491029e69d21dbd6
sejinbaek <sejin9767@gmail.com>
2025-07-20 00:45:15 +0900

    refactor: FeatureSection 속 PurpleCircle 배치

--- src/features/landingPage/components/common/PurpleCircle.tsx
+++ src/features/landingPage/components/common/PurpleCircle.tsx
@@ -4,8 +4,7 @@ const PurpleCircle = forwardRef<HTMLDivElement>((_, ref) => {
   return (
     <div
       ref={ref}
-      className="circle w-[80vw] h-[80vw] rounded-full bg-purple04 fixed top-1/2 left-1/2 
-      -translate-x-1/2 -translate-y-1/2 z-30 pointer-events-none"
+      className="w-32 h-32 rounded-full bg-purple04 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 pointer-events-none z-30"
     />
   );
 });
--- src/features/landingPage/components/sections/FeatureSection.tsx
+++ src/features/landingPage/components/sections/FeatureSection.tsx
@@ -1,7 +1,45 @@
+import { useRef } from 'react';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+import PurpleCircle from '../common/PurpleCircle';
+
+gsap.registerPlugin(ScrollTrigger);
+
 const FeatureSection = () => {
+  const circleRef = useRef<HTMLDivElement>(null);
+  const sectionRef = useRef<HTMLDivElement>(null);
+
+  // GSAP 애니메이션
+  useGSAP(() => {
+    if (!circleRef.current || !sectionRef.current) return;
+
+    gsap.fromTo(
+      circleRef.current,
+      { scale: 0.3 },
+      {
+        scale: 30,
+        scrollTrigger: {
+          trigger: sectionRef.current,
+          start: 'center center',
+          end: '+=500',
+          scrub: 0.5,
+          pin: true,
+          markers: true, // 개발 후 삭제하기
+        },
+        ease: 'none',
+      }
+    );
+  }, []);
+
   return (
-    <div className="w-full h-screen flex justify-center items-center text-white text-8xl bg-gray-300">
+    <div
+      ref={sectionRef}
+      className="relative w-full h-screen flex justify-center items-center text-white text-8xl bg-black overflow-hidden"
+    >
       기능 설명 섹션입니다
+      <PurpleCircle ref={circleRef} />
     </div>
   );
 };
