05db2a7c0b9d04de6b24200f554a4aec394650b1
sejinbaek <sejin9767@gmail.com>
2025-07-19 13:46:50 +0900

    feat: 지도 & 기능 설명 섹션 추가

---
+++ src/features/landingPage/components/sections/FeatureSection.tsx
@@ -0,0 +1,9 @@
+const FeatureSection = () => {
+  return (
+    <div className="w-full h-screen flex justify-center items-center text-white text-8xl bg-gray-300">
+      기능 설명 섹션입니다
+    </div>
+  );
+};
+
+export default FeatureSection;
---
+++ src/features/landingPage/components/sections/MapSection.tsx
@@ -0,0 +1,9 @@
+const MapSection = () => {
+  return (
+    <div className="w-full h-screen flex justify-center items-center text-white text-8xl bg-gray-600">
+      지도 섹션입니다
+    </div>
+  );
+};
+
+export default MapSection;
