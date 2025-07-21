10cce76951f1200ce9c29c279103f5aee272a7f7
Yeom-Seung-Ah <ysa2780@naver.com>
2025-07-21 17:49:23 +0900

    feat: 혜택 사용 이력 mock데이터 추가

---
+++ src/features/myPage/mock/mockHistory.ts
@@ -0,0 +1,45 @@
+// src/features/myPage/mock/mockHistory.ts
+export const mockHistory = [
+  {
+    image: '/images/mock/gs25.png',
+    benefitName: 'GS25',
+    discountAmount: 4500,
+    usedAt: '2025-07-03 11:23 AM',
+  },
+  {
+    image: '/images/mock/gs25.png',
+    benefitName: 'GS25',
+    discountAmount: 3800,
+    usedAt: '2025-07-01 18:48 PM',
+  },
+  {
+    image: '/images/mock/gs25.png',
+    benefitName: 'GS25',
+    discountAmount: 3800,
+    usedAt: '2025-06-30 17:00 PM',
+  },
+  {
+    image: '/images/mock/gs25.png',
+    benefitName: 'GS25',
+    discountAmount: 3800,
+    usedAt: '2025-06-28 16:22 PM',
+  },
+  {
+    image: '/images/mock/gs25.png',
+    benefitName: 'GS25',
+    discountAmount: 3800,
+    usedAt: '2025-06-27 13:05 PM',
+  },
+  {
+    image: '/images/mock/gs25.png',
+    benefitName: 'GS25',
+    discountAmount: 3800,
+    usedAt: '2025-06-20 10:15 PM',
+  },
+  {
+    image: '/images/mock/gs25.png',
+    benefitName: 'GS25',
+    discountAmount: 3800,
+    usedAt: '2025-06-19 11:35 AM',
+  },
+];
