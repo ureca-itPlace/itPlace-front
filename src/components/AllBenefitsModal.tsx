fcc7420131309e735b2ecfe7a2784d0b28831e65
udwns310 <uroasecret5@gmail.com>
2025-07-24 11:36:19 +0900

    style : 혜택 상세 정보 모달 반응형

--- src/components/AllBenefitsModal.tsx
+++ src/components/AllBenefitsModal.tsx
@@ -6,35 +6,25 @@ interface ModalProps {
   onClose: () => void;
   title: string;
   children: React.ReactNode;
-  width?: number;
-  height?: number;
 }
 
-const Modal: React.FC<ModalProps> = ({
-  isOpen,
-  onClose,
-  title,
-  children,
-  width = 800,
-  height = 664,
-}) => {
+const Modal: React.FC<ModalProps> = ({ isOpen, onClose, title, children }) => {
   if (!isOpen) return null;
 
   return (
     <div
-      className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
+      className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4 max-md:p-2"
       onClick={onClose}
     >
       <div
-        className="bg-white rounded-[20px] relative overflow-hidden"
-        style={{ width, height }}
+        className="bg-white rounded-[20px] relative overflow-hidden w-full h-full max-w-[800px] max-h-[664px] max-md:max-w-[340px] max-md:max-h-[80vh] max-md:rounded-[16px]"
         onClick={(e) => e.stopPropagation()}
       >
         {/* 모달 헤더 */}
-        <div className="bg-grey01 rounded-t-[20px] flex items-center justify-between pl-8 pt-6 pb-4 pr-4">
-          <h3 className="text-title-5  ">{title}</h3>
+        <div className="bg-grey01 rounded-t-[20px] max-md:rounded-t-[16px] flex items-center justify-between pl-8 max-md:pl-4 pt-6 max-md:pt-4 pb-4 max-md:pb-3 pr-4 max-md:pr-3">
+          <h3 className="text-title-5 max-md:text-title-7">{title}</h3>
           <button onClick={onClose} className="text-grey05">
-            <TbX size={36} />
+            <TbX size={36} className="max-md:w-8 max-md:h-8" />
           </button>
         </div>
 
