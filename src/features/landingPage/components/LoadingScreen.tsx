9c531d9f6d99bf187c5548074f5d635ae380a077
sejinbaek <sejin9767@gmail.com>
2025-07-24 23:12:09 +0900

    feat: 로딩 반응형 처리

--- src/features/landingPage/components/LoadingScreen.tsx
+++ src/features/landingPage/components/LoadingScreen.tsx
@@ -2,7 +2,7 @@ import { useProgress } from '@react-three/drei';
 import { gsap } from 'gsap';
 import { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import { useGSAP } from '@gsap/react';
-import { LoadingScreenAnimation } from '../animations/LoadingScreenAnimation';
+import { useResponsive } from '../../../hooks/useResponsive';
 
 const LoadingScreen = () => {
   const { progress, active } = useProgress();
@@ -11,6 +11,10 @@ const LoadingScreen = () => {
   const logoRef = useRef<HTMLHeadingElement | null>(null);
   const descRef = useRef<HTMLParagraphElement | null>(null);
   const bgRef = useRef<HTMLDivElement | null>(null);
+
+  const { isMobile } = useResponsive();
+
+  const descFadeOutDistance = isMobile ? '-20' : '0';
 
   useEffect(() => {
     if (isVisible) {
@@ -51,7 +55,57 @@ const LoadingScreen = () => {
     () => {
       if (!logoRef.current || !descRef.current || !bgRef.current) return;
 
-      LoadingScreenAnimation({ logoRef, descRef, bgRef });
+      const tl = gsap.timeline();
+
+      if (logoRef.current && descRef.current && bgRef.current) {
+        // 1. 로고 & 멘트 위로 이동하며 등장
+        tl.to(
+          logoRef.current,
+          {
+            y: -40,
+            ease: 'power1.out',
+            zIndex: 30,
+            delay: 0.5,
+          },
+          'sync'
+        )
+          .fromTo(
+            descRef.current,
+            { opacity: 0, y: 40 },
+            {
+              opacity: 1,
+              y: descFadeOutDistance,
+              ease: 'power2.out',
+              zIndex: 30,
+              delay: 0.5,
+            },
+            'sync'
+          )
+          // 2. 텍스트 색상 하얀색, 배경 색상 검정색으로 변경
+          .add(() => {
+            gsap.to(logoRef.current, { color: 'white' });
+            gsap.to(descRef.current, { color: 'white' });
+            gsap.to(bgRef.current, { backgroundColor: 'black' });
+          }, '+=0.1')
+
+          // 3. 로고 & 멘트 fade out
+          .add(() => {
+            gsap.to(logoRef.current, {
+              y: 0,
+              opacity: 0,
+              scale: 0.7,
+              duration: 0.5,
+              ease: 'power3.out',
+            });
+            gsap.to(descRef.current, {
+              y: 0,
+              opacity: 0,
+              scale: 0.7,
+              duration: 0.5,
+              ease: 'power3.out',
+            });
+          }, '+=1');
+      }
     },
     { scope: bgRef }
   );
