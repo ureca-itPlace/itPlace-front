14c7989b1924e5c02b9ad80378387e87fa44dc4f
박용규 <yoyo5411@naver.com>
2025-07-27 23:18:41 +0900

    style : LoginForm 모바일 버전 높이 조절

--- src/features/loginPage/layouts/AuthLayout.tsx
+++ src/features/loginPage/layouts/AuthLayout.tsx
@@ -262,10 +262,10 @@ const AuthLayout = () => {
         </div>
 
         {/* 하단에서 올라오는 오버레이 폼 카드 - 전체 너비 */}
-        <div className="absolute bottom-0 left-0 w-full" style={{ bottom: '0rem' }}>
+        <div className="absolute left-0 w-full" style={{ bottom: '0', top: 'calc(50vh - 13rem)' }}>
           <div
-            className="bg-white rounded-t-[24px] px-6 pt-8 pb-8 w-full shadow-2xl overflow-hidden"
-            style={{ minHeight: 'calc(50vh + 5rem)' }}
+            className="bg-white rounded-t-[24px] px-6 pt-8 pb-8 w-full shadow-2xl overflow-hidden h-full"
+            style={{ minHeight: 'calc(50vh + 3rem)' }}
           >
             {/* 로그인 */}
             {formStep === 'login' && (
