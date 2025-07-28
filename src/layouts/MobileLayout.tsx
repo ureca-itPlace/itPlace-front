8541b1b3ef5db2a62f96d514442be1088034222c
Yeom-Seung-Ah <ysa2780@naver.com>
2025-07-28 09:39:46 +0900

    fix: 두번 겹쳐있는 모바일 헤더 제거

--- src/layouts/MobileLayout.tsx
+++ src/layouts/MobileLayout.tsx
@@ -1,11 +1,9 @@
 // src/layouts/MobileLayout.tsx
 import { Outlet } from 'react-router-dom';
-import MobileHeader from '../components/MobileHeader';
 
 const MobileLayout = () => {
   return (
     <div className="max-md:block hidden bg-white min-h-screen">
-      <MobileHeader />
       <main className="px-5">
         <Outlet />
       </main>
