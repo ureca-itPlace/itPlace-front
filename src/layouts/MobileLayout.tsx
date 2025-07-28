1ee35d2387e71b2403ae0845c293dced7bc72836
Yeom-Seung-Ah <ysa2780@naver.com>
2025-07-28 10:42:27 +0900

    fix: 모바일 헤더 배경이 투명하게 보이는 버그 해결

--- src/layouts/MobileLayout.tsx
+++ src/layouts/MobileLayout.tsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom';
 const MobileLayout = () => {
   return (
     <div className="max-md:block hidden bg-white min-h-screen">
-      <main className="px-5">
+      <main className="px-5 mt-[54px]">
         <Outlet />
       </main>
     </div>
