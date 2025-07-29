9b75380f55ed974c153fff557aceae09b30dfa12
Yeom-Seung-Ah <ysa2780@naver.com>
2025-07-29 17:40:05 +0900

    fix: 상세혜택탭 모바일 오류 전면 수정 완료

--- src/features/mainPage/components/SidebarSection/StoreDetail/index.tsx
+++ src/features/mainPage/components/SidebarSection/StoreDetail/index.tsx
@@ -113,13 +113,13 @@ const StoreDetailCard: React.FC<StoreDetailCardProps> = ({ platform, onClose }) 
 
       {/* 스크롤 영역 - 이용 방법만 */}
       <div
-        className={`flex-1 overflow-y-auto ${detailData?.data.manual ? 'px-6 max-md:px-4' : ''}`}
+        className={`flex-1 overflow-y-auto pb-6 max-md:overflow-y-visible ${detailData?.data.manual ? 'px-6 max-md:px-4' : ''}`}
       >
         <StoreDetailUsageGuide detailData={detailData} />
       </div>
 
       {/* 고정 버튼 */}
-      <div className="px-6 pb-2 mt-2 flex-shrink-0 max-md:px-4 max-md:pb-2">
+      <div className="px-6 pb-2 mt-2 flex-shrink-0 max-md:px-4 max-md:pb-2 max-md:fixed max-md:bottom-0 max-md:w-full max-md:bg-white">
         <StoreDetailActionButton
           benefitId={detailData?.data?.benefitId}
           isFavorite={isFavorite}
