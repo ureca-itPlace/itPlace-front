58f30858dc3d4727402219f8b9888eec256f7d18
sejinbaek <sejin9767@gmail.com>
2025-08-01 15:05:57 +0900

    feat: CTA섹션 애니메이션 적용

--- src/features/landingPage/sections/StartCTASection.tsx
+++ src/features/landingPage/sections/StartCTASection.tsx
@@ -2,10 +2,53 @@ import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../../store';
 import Button from '../components/Button';
+import gsap from 'gsap';
+import { useEffect, useRef } from 'react';
 
 const StartCTASection = () => {
   const navigate = useNavigate();
   const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
+  const imageRef = useRef<HTMLImageElement>(null);
+  const textRef = useRef<HTMLHeadingElement>(null);
+  const buttonRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const text = textRef.current;
+    if (text) {
+      gsap.fromTo(
+        text,
+        { scale: 0.5, opacity: 0 },
+        { scale: 1, opacity: 1, duration: 1, ease: 'power2.inOut' }
+      );
+    }
+
+    const button = buttonRef.current;
+    if (button) {
+      gsap.fromTo(
+        button,
+        { scale: 0, opacity: 0 },
+        { scale: 1, opacity: 1, duration: 1, ease: 'power2.inOut' }
+      );
+    }
+
+    const image = imageRef.current;
+    if (image) {
+      gsap.fromTo(
+        image,
+        { x: '-100%', opacity: 0 },
+        {
+          x: '0%',
+          opacity: 1,
+          duration: 1,
+          ease: 'power2.out',
+          delay: 0.5,
+          onComplete: () => {
+            gsap.to(image, { y: -20, yoyo: true, repeat: -1, duration: 0.7, ease: 'sine.out' });
+          },
+        }
+      );
+    }
+  }, []);
 
   return (
     <section
@@ -14,6 +57,7 @@ const StartCTASection = () => {
     >
       <div className="absolute left-[-18%] top-[5%] w-[45%] h-[85%] max-lg:hidden">
         <img
+          ref={imageRef}
           src="/images/landing/cta/cta-rabbit.webp"
           alt="CTA토끼"
           loading="lazy"
@@ -22,13 +66,19 @@ const StartCTASection = () => {
       </div>
 
       <div className="text-center z-10">
-        <h1 className="custom-font text-[200px] text-purple04 drop-shadow-[0_10px_10px_rgba(0,0,0,0.3)] tracking-wide max-xl:text-[130px] max-sm:text-[52px]">
+        <h1
+          ref={textRef}
+          className="custom-font text-[200px] text-purple04 drop-shadow-[0_10px_10px_rgba(0,0,0,0.3)] tracking-wide max-xl:text-[130px] max-sm:text-[52px]"
+        >
           LET’S START
           <br />
           IT PLACE!
         </h1>
 
-        <div className="mt-10 flex justify-center items-center gap-6 max-md:flex-col">
+        <div
+          ref={buttonRef}
+          className="mt-10 flex justify-center items-center gap-6 max-md:flex-col"
+        >
           {!isLoggedIn && (
             <Button variant="outline" onClick={() => navigate('/login')}>
               로그인하기
