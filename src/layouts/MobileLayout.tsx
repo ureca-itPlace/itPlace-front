fee22174211f9af4d168e32dce4c0fcc1bf3ab9c
박용규 <yoyo5411@naver.com>
2025-08-02 04:07:45 +0900

    style : 모바일 레이아웃 수정

--- src/layouts/MobileLayout.tsx
+++ src/layouts/MobileLayout.tsx
@@ -4,16 +4,10 @@ import { Outlet, useLocation } from 'react-router-dom';
 const MobileLayout = () => {
   const location = useLocation();
   const isAllBenefitsPage = location.pathname === '/benefits';
-  const isMainPage = location.pathname === '/main';
 
   return (
     <div className="max-md:block hidden bg-white min-h-screen">
-      <main
-        className={`
-          ${isMainPage ? '' : 'mt-[54px]'} 
-          ${isAllBenefitsPage || isMainPage ? '' : 'px-5'}
-        `}
-      >
+      <main className={`${isAllBenefitsPage ? '' : 'px-5'} mt-[54px]`}>
         <Outlet />
       </main>
     </div>
