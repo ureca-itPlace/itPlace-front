3980ef3ae62ebb348f38e8323d5de8faaf2f8e6b
sejinbaek <sejin9767@gmail.com>
2025-08-02 17:42:28 +0900

    feat: 오늘 하루동안 보지 않기 구현

--- src/features/eventPage/components/Modal/StarModal.tsx
+++ src/features/eventPage/components/Modal/StarModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Modal from '../../../../components/Modal';
 import StarImg from '/images/event/event-star.png';
 import EventModalButton from './EventModalButton';
@@ -8,7 +8,31 @@ interface StarModalProps {
   onClose: () => void;
 }
 
+const getTodayString = () => {
+  const today = new Date();
+  return today.toISOString().split('T')[0]; // YYYY-MM-DD 형식으로 반환
+};
+
 const StarModal: React.FC<StarModalProps> = ({ isOpen, onClose }) => {
+  const [isHidden, setIsHidden] = useState<boolean>(false);
+
+  useEffect(() => {
+    const hiddenDate = localStorage.getItem('starModalHiddenDate');
+    if (hiddenDate === getTodayString()) {
+      setIsHidden(true);
+    }
+  }, []);
+
+  const handleHide = () => {
+    localStorage.setItem('starModalHiddenDate', getTodayString()); // 오늘 날짜를 로컬 스토리지에 저장
+    setIsHidden(true); // 상태 업데이트
+    onClose(); // 모달 닫기
+  };
+
+  if (isHidden) {
+    return null; // 모달이 숨겨져 있으면 아무것도 렌더링하지 않음
+  }
+
   return (
     <Modal isOpen={isOpen} onClose={onClose} widthClass="w-[624px] max-md:max-w-[calc(100%-32px)]">
       <div className="flex flex-col items-center text-center">
@@ -25,8 +49,8 @@ const StarModal: React.FC<StarModalProps> = ({ isOpen, onClose }) => {
           <br /> 사용 이력을 등록하면 이벤트에 참여할 수 있어요.
         </p>
         <EventModalButton onClick={onClose} label="확인" />
-        <button onClick={onClose} className="underline mt-6 text-body-3 text-grey03 font-light">
-          다시 보지 않기
+        <button onClick={handleHide} className="underline mt-6 text-body-3 text-grey03 font-light">
+          오늘 하루 보지 않기
         </button>
       </div>
     </Modal>
